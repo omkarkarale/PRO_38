b7bad41a626abe633a2a3e3e2b7afb37af369f4e
omkarkarale <82709945+omkarkarale@users.noreply.github.com>
2021-08-10 00:49:33 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -13,7 +13,7 @@ class Form {
         this.title.hide();
     }
     display(){
-        this.title.html("Car Racing Game");
+        this.title.html("Cycle Racing Game");
         this.title.position(displayWidth/2 - 50, 0);
 
         this.input.position(displayWidth/2 - 40, displayHeight/2 - 80);
